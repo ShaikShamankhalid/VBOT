cfd7a86fce7c876ccec39c4d42f13132c133909c
sksmnkld <124290423+ShaikShamankhalid@users.noreply.github.com>
2026-01-08 22:07:41 +0530

    Add files via upload

---
+++ src/RAG/pipelines/stage_01_data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import random
+from RAG.components.data_ingestion import DataIngestion
+from RAG.config.configuration import ConfigurationManager
+from RAG import logger
+from RAG.utils.common import setup_env
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionPipeLine:
+    def __init__(self) -> None:
+        setup_env()
+        config = ConfigurationManager()
+        self.data_ingestion_config = config.get_data_ingestion_config()
+        self.pc = ""
+        
+    def main(self):
+        data_ingestion = DataIngestion(self.data_ingestion_config, self.pc)
+        data_ingestion.download_data()
+        pages_and_text = data_ingestion.open_and_read_pdf()
+        pages_and_text = data_ingestion.sentencize_and_chunk(pages_and_text)
+        pages_and_chunks = data_ingestion.split_chunks(pages_and_text)
+        pages_and_chunks_over_min_token_len = data_ingestion.embed_chunks(pages_and_chunks)
+        # data_ingestion.to_vector_database(pages_and_chunks_over_min_token_len)
+        return pages_and_chunks_over_min_token_len
+
+    def load_to_pincone(self, api_key, file_path, index_name):
+        data_ingestion = DataIngestion(self.data_ingestion_config, self.pc)
+        pages_and_text = data_ingestion.open_and_read_pdf(file_path)
+        pages_and_text = data_ingestion.sentencize_and_chunk(pages_and_text)
+        pages_and_chunks = data_ingestion.split_chunks(pages_and_text, file_path)
+        pages_and_chunks_over_min_token_len = data_ingestion.embed_chunks(pages_and_chunks)
+        data_ingestion.to_vector_database(pages_and_chunks_over_min_token_len, index_name)
+
+    def set_pc(self, pc):
+        self.pc = pc
+
+    def store_tokens(self, api_key, file_path, index_name):
+        data_ingestion = DataIngestion(self.data_ingestion_config, api_key)
+        pages_and_text = data_ingestion.open_and_read_pdf(file_path)
+        pages_and_text = data_ingestion.sentencize_and_chunk(pages_and_text)
+        pages_and_chunks = data_ingestion.split_chunks(pages_and_text, file_path)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>> {STAGE_NAME} Started <<<<<<<<<<<<")
+        obj = DataIngestionPipeLine()
+        pages_and_text = obj.main()
+        # print(random.sample(pages_and_text, k=1))
+        logger.info(f">>>>>>>> {STAGE_NAME} Completed <<<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/RAG/pipelines/stage_02_search_answer.py
@@ -0,0 +1,76 @@
+from RAG.components.search_and_answer import SearchAndAnswer
+from RAG.config.configuration import ConfigurationManager
+from RAG import logger
+from RAG.utils.prompts import PROMPTS
+from RAG.utils.common import setup_env
+
+STAGE_NAME = "Search Answer stage"
+
+
+class SearchAnswerPipeline:
+    def __init__(self, llm_model=None, tokenizer=None, embed_model=None) -> None:
+        setup_env()
+        config = ConfigurationManager()
+        self.search_config = config.get_search_config()
+        self.query_answer = SearchAndAnswer(
+            self.search_config, llm_model, tokenizer, embed_model
+        )
+
+    def main(self, query: str, prompt: str):
+        query_embeddings = self.query_answer.retrive_similar_enbeddings(query)
+        query_results = self.query_answer.fetch_chunks(query_embeddings)
+        output_text = self.query_answer.ask(query, query_results, prompt)
+        return output_text
+
+    def chainlit_prompt(self, query: str, prompt: str, hf_key, index, model):
+        try:
+            if index != "chat":
+                query_embeddings = self.query_answer.retrive_similar_enbeddings(
+                    query, index
+                )
+                query_results = self.query_answer.fetch_chunks(query_embeddings, index)
+            else:
+                query_results = []
+            output_text = self.query_answer.ask(
+                query, query_results, prompt, hf_key, model, index
+            )
+            return output_text
+        except Exception as e:
+            print(e)
+            return "data or pdf not loaded in the slot or " + str(e)
+
+    def set_pc(self, pc):
+        self.query_answer.pc = pc
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>> {STAGE_NAME} Started <<<<<<<<<<<<")
+        obj = SearchAnswerPipeline("Pc-key...")
+        query = ""
+        prompt = "detailed_prompt"
+        obj.query_answer.setup_pd(
+            {
+                "slot-1": obj.search_config.data_file,
+                "rag-index": obj.search_config.data_file,
+                "slot-3": "",
+                "slot-4": "",
+                "slot-5": "",
+            }
+        )
+        obj.chainlit_prompt(
+            "how many bones does our body has?",
+            prompt,
+            "",
+            "",
+            "rag-index",
+            "google/gemma-2b-it",
+        )
+        # while query != ".exit":
+        #     query = input("Enter The Query: ")
+        #     output_text = obj.main(query, prompt)
+        #     print(output_text)
+        # logger.info(f">>>>>>>> {STAGE_NAME} Completed <<<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
